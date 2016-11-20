56e5469871f17faac3534937c140a0d482e5a7ca
Mia0227 <1103104218@gm.kuas.edu.tw>
2016-11-21 00:41:18 +0800

    button

--- button.c
+++ button.c
@@ -6,7 +6,7 @@ int main(void){
 	
 	SystemInit();								//initial system
   T16B0_init();								//initial timer
-	led_init();									//set the P1.8 output pin & set high to P1.8  
+	LED_INIT(1,8);									//set the P1.8 output pin & set high to P1.8  
 	LPC_GPIO2->DIR &=~ (1<<0);  //set the P2.0 intput pin
 	
 	while(1){
@@ -16,15 +16,15 @@ int main(void){
 				T16B0_delay_ms(250);							  //delay 0.25s
 				while((LPC_GPIO2->DATA&(1<<0))==0){	//while the button(P2.0) is still pressed
 					T16B0_delay_ms(250);							//delay 0.25s
-					led_state();											//change the led(P1.8) state
+					LED_STATUS(1,8);											//change the led(P1.8) state
 					i=1;															//Long press i=1
 				}
 			}
 			if(i==1){                             //if(long press)
-			led_off();														//set up led(P1.8) led off
+			LED_OFF(1,8);														//set up led(P1.8) led off
 			}
 			else																	//else   ->Short press
-			led_state();													//change the led(P1.8) state
+			LED_STATUS(1,8);														//change the led(P1.8) state
 		}
 	}
 }
