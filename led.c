1bd8cc9ca44e7aef86786ab228cdf11b86c981a9
Mia0227 <1103104218@gm.kuas.edu.tw>
2016-11-25 15:07:51 +0800

    timer

--- led.c
+++ led.c
@@ -2,22 +2,17 @@
 #include "timer16.h"
 #include "LED2.h"
 
-void init()
-{
-   LPC_SYSCON->SYSAHBCLKCTRL |= (1<<16);   //enable IOCON clock
-   LPC_SYSCON->SYSAHBCLKCTRL &= ~(1<<16);  //disable IOCON clock
-	 LED_INIT(1,8);													 //set the P1.8 output pin  
-}
+
 int main()
 {
-   init();
+   LED_INIT();										//set the P1.8 output pin
    T16B0_init();           				//initial timer
    while(1)
    { 
-      LED_ON(1,8);					 	 		//set up led(P1.8) led on
+      LED_ON();					 	 		    //set up led(P1.8) led on
       T16B0_delay_ms(1000);				//delay 1s
       
-      LED_OFF(1,8);								//set up led(P1.8) led off
+      LED_OFF();								  //set up led(P1.8) led off
       T16B0_delay_ms(250);				//delay 0.25s
    }
 }
